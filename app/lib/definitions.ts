9be121e73bac5c6480e116e8b45b15245353f3fe
Look! <lukehsu1999@gmail.com>
2024-01-07 21:26:16 -0500

    customize basic data type (user, sharingPost) in definitions

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -9,80 +9,94 @@ export type User = {
   password: string;
 };
 
-export type Customer = {
+export type SharingPost = {
   id: string;
-  name: string;
-  email: string;
-  image_url: string;
+  creator_id: string;
+  creation_date: string;
+  company: string;
+  interview_status: 'Phone interview' | 'Online Assessment' | 'First Round' | 'Second Round' | 'Third Round' | 'Final Round' | 'Rejected' | 'Offered' | 'Others';
+  interview_type: 'Technical Interview' | 'Behavioral Interview' | 'Case Interview' | 'Others';
+  title: string;
+  content: string;
+  likes: number;
+  views: number;
 };
 
-export type Invoice = {
-  id: string;
-  customer_id: string;
-  amount: number;
-  date: string;
-  // In TypeScript, this is called a string union type.
-  // It means that the "status" property can only be one of the two strings: 'pending' or 'paid'.
-  status: 'pending' | 'paid';
-};
 
-export type Revenue = {
-  month: string;
-  revenue: number;
-};
+// export type Customer = {
+//   id: string;
+//   name: string;
+//   email: string;
+//   image_url: string;
+// };
 
-export type LatestInvoice = {
-  id: string;
-  name: string;
-  image_url: string;
-  email: string;
-  amount: string;
-};
+// export type Invoice = {
+//   id: string;
+//   customer_id: string;
+//   amount: number;
+//   date: string;
+//   // In TypeScript, this is called a string union type.
+//   // It means that the "status" property can only be one of the two strings: 'pending' or 'paid'.
+//   status: 'pending' | 'paid';
+// };
 
-// The database returns a number for amount, but we later format it to a string with the formatCurrency function
-export type LatestInvoiceRaw = Omit<LatestInvoice, 'amount'> & {
-  amount: number;
-};
+// export type Revenue = {
+//   month: string;
+//   revenue: number;
+// };
 
-export type InvoicesTable = {
-  id: string;
-  customer_id: string;
-  name: string;
-  email: string;
-  image_url: string;
-  date: string;
-  amount: number;
-  status: 'pending' | 'paid';
-};
+// export type LatestInvoice = {
+//   id: string;
+//   name: string;
+//   image_url: string;
+//   email: string;
+//   amount: string;
+// };
 
-export type CustomersTableType = {
-  id: string;
-  name: string;
-  email: string;
-  image_url: string;
-  total_invoices: number;
-  total_pending: number;
-  total_paid: number;
-};
+// // The database returns a number for amount, but we later format it to a string with the formatCurrency function
+// export type LatestInvoiceRaw = Omit<LatestInvoice, 'amount'> & {
+//   amount: number;
+// };
 
-export type FormattedCustomersTable = {
-  id: string;
-  name: string;
-  email: string;
-  image_url: string;
-  total_invoices: number;
-  total_pending: string;
-  total_paid: string;
-};
+// export type InvoicesTable = {
+//   id: string;
+//   customer_id: string;
+//   name: string;
+//   email: string;
+//   image_url: string;
+//   date: string;
+//   amount: number;
+//   status: 'pending' | 'paid';
+// };
 
-export type CustomerField = {
-  id: string;
-  name: string;
-};
+// export type CustomersTableType = {
+//   id: string;
+//   name: string;
+//   email: string;
+//   image_url: string;
+//   total_invoices: number;
+//   total_pending: number;
+//   total_paid: number;
+// };
 
-export type InvoiceForm = {
-  id: string;
-  customer_id: string;
-  amount: number;
-  status: 'pending' | 'paid';
-};
+// export type FormattedCustomersTable = {
+//   id: string;
+//   name: string;
+//   email: string;
+//   image_url: string;
+//   total_invoices: number;
+//   total_pending: string;
+//   total_paid: string;
+// };
+
+// export type CustomerField = {
+//   id: string;
+//   name: string;
+// };
+
+// export type InvoiceForm = {
+//   id: string;
+//   customer_id: string;
+//   amount: number;
+//   status: 'pending' | 'paid';
+// };
