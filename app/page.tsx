68a7f2820108177c4de90f0c7a5633fa1fe77b2c
Look! <lukehsu1999@gmail.com>
2024-01-08 10:48:36 -0500

    Landing Page layout

--- app/page.tsx
+++ app/page.tsx
@@ -11,11 +11,8 @@ export default function Page() {
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
-            </a>
-            , brought to you by Vercel.
+            <strong>Welcome to Interview Experience Sharing Platform.</strong> This is a prototype for letting people to create sharing posts on interview experience, 
+            regardless of the company, position, and industry.
           </p>
           <Link
             href="/login"
