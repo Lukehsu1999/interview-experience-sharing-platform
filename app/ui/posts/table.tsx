1e680e52d41f418a73a5fa3669a9f65d0c8bc4d9
Look! <lukehsu1999@gmail.com>
2024-01-10 13:54:12 -0500

    add debounce

--- app/ui/posts/table.tsx
+++ app/ui/posts/table.tsx
@@ -89,11 +89,6 @@ export default async function InvoicesTable({
                   key={post.id}
                   className="w-full border-b py-3 text-sm last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg"
                 >
-                  {/* <td className="whitespace-nowrap py-3 pl-6 pr-3">
-                    <div className="flex items-center gap-3">
-                      <p>{post.name}</p>
-                    </div>
-                  </td> */}
                   <td className="whitespace-nowrap px-3 py-3">
                     {post.company}
                   </td>
