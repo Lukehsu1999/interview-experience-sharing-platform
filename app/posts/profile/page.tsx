6966982cb3026778c8982bd5296b6fd8c6f46188
Look! <lukehsu1999@gmail.com>
2024-02-08 14:55:06 -0500

    add CreatePost to personal profile page

--- app/posts/profile/page.tsx
+++ app/posts/profile/page.tsx
@@ -4,6 +4,7 @@ import { Card } from '@/app/ui/dashboard/cards';
 import { lusitana } from '@/app/ui/fonts';
 import { Suspense } from 'react';
 import { RevenueChartSkeleton } from '@/app/ui/skeletons';
+import { CreatePost } from '@/app/ui/posts/buttons';
 import {
   LatestInvoicesSkeleton,
   CardsSkeleton,
@@ -42,7 +43,7 @@ export default async function Page({
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Dashboard
       </h1>
-      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
         <Suspense fallback={<CardsSkeleton />}>
           <CardWrapper userId={userId} />
         </Suspense>
@@ -50,13 +51,16 @@ export default async function Page({
       <h1 className={`${lusitana.className} mt-4 mb-4 text-xl md:text-2xl`}>
         My Posts
       </h1>
-      <div className="grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+      <div className="grid grid-cols-1 gap-6">
         <Suspense
           key={query + currentPage}
           fallback={<InvoicesTableSkeleton />}
         >
           <Table query={userId} />
         </Suspense>
+      </div>
+      <div className="mt-2 grid grid-cols-1">
+        <CreatePost />
       </div>
     </main>
   );
