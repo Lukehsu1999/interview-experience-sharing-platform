28a19c828471ce5ecd816a38376989345ffb0136
Look! <lukehsu1999@gmail.com>
2024-02-09 13:27:20 -0500

    color change for posts page

--- app/ui/posts/buttons.tsx
+++ app/ui/posts/buttons.tsx
@@ -5,7 +5,7 @@ export function CreateInvoice() {
   return (
     <Link
       href="/dashboard/invoices/create"
-      className="flex h-10 items-center rounded-lg bg-blue-600 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+      className="flex h-10 items-center rounded-lg bg-miumee-color-600 px-4 text-sm font-medium text-white transition-colors hover:bg-miumee-color-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-miumee-color-600"
     >
       <span className="hidden md:block">Create Invoice</span>{' '}
       <PlusIcon className="h-5 md:ml-4" />
@@ -17,7 +17,7 @@ export function CreatePost() {
   return (
     <Link
       href="/posts/create"
-      className="flex h-10 items-center justify-center rounded-lg bg-blue-600 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+      className="flex h-10 items-center justify-center rounded-lg bg-miumee-color-500 px-4 text-sm font-medium text-white transition-colors hover:bg-miumee-color-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-miumee-color-500"
     >
       <span className="">Create Post</span>{' '}
       <PlusIcon className="h-5 md:ml-4" />
@@ -40,7 +40,7 @@ export function ViewPost({ id }: { id: string }) {
   return (
     <Link
       href={`/posts/${id}/view`}
-      className="flex h-10 items-center rounded-lg bg-blue-600 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+      className="flex h-10 items-center rounded-lg bg-miumee-color-600 px-4 text-sm font-medium text-white transition-colors hover:bg-miumee-color-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-miumee-color-600"
     >
       <EyeIcon className="w-5" />{' '}
       <span className="hidden md:block">View</span>
