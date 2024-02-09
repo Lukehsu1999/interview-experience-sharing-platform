b59758f09b1318221f89ce9000abc95c19ab9e84
Look! <lukehsu1999@gmail.com>
2024-02-09 13:52:16 -0500

    swap icons for nav links

--- app/ui/posts/nav-links.tsx
+++ app/ui/posts/nav-links.tsx
@@ -11,8 +11,8 @@ import clsx from 'clsx';
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
-  { name: 'Posts', href: '/posts', icon: HomeIcon },
-  { name: 'Profile', href: '/posts/profile', icon: UserGroupIcon},
+  { name: 'Posts', href: '/posts', icon: UserGroupIcon },
+  { name: 'Profile', href: '/posts/profile', icon: HomeIcon},
   {
     name: 'About',
     href: '/posts/about',
