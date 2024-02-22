ed415229a8c11a3e1da1a516fb5bb20611501907
Lukehsu <lukehsu@Lukes-MacBook-Air-2.local>
2024-02-22 10:59:56 -0500

    change create post content prompt

--- app/ui/posts/create-form.tsx
+++ app/ui/posts/create-form.tsx
@@ -152,7 +152,7 @@ export default function Form({ userId }: FormProps) {
                 id="content"
                 name="content"
                 ref={textareaRef}
-                placeholder="Share your experience here~"
+                placeholder="Share your experience here~ a paragraph or two will be appreciated"
                 onChange={handleContentChange}
                 className="w-full border border-gray-300 p-2 rounded-md placeholder:text-gray-500"
                 style={{ minHeight: '150px' }}
