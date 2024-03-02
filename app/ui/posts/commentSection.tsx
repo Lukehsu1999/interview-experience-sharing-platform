19ea7033b26670ee38bde6a4a0d75b1e2cf631ee
Lukehsu <lukehsu1999@gmail.com>
2024-03-02 00:04:20 -0500

    add style to commentSection

--- app/ui/posts/commentSection.tsx
+++ app/ui/posts/commentSection.tsx
@@ -1,8 +1,9 @@
 'use client';
 import { Comment } from '@/app/lib/definitions';
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { createComment } from '@/app/lib/actions';
-import { set } from 'zod';
+import { Button } from '@/app/ui/button';
+import { formatDateToLocal } from '@/app/lib/utils';
 
 export function CommentSection({
   post_id,
@@ -14,46 +15,68 @@ export function CommentSection({
   user_id: string;
 }) {
   const [newComment, setNewComment] = useState('');
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+  useEffect(() => {
+    if (textareaRef.current) {
+      textareaRef.current.style.height = 'auto';
+      textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
+    }
+  }, [newComment]);
+  const handleContentChange = (
+    event: React.ChangeEvent<HTMLTextAreaElement>,
+  ) => {
+    setNewComment(event.target.value);
+  };
 
   const addCommentEvent = async () => {
     try {
-        console.log('Adding new comment:', newComment);
-        const res = await createComment(post_id, user_id, newComment);
-        setNewComment('');
+      console.log('Adding new comment:', newComment);
+      const res = await createComment(post_id, user_id, newComment);
+      setNewComment('');
     } catch (err) {
       console.log(err);
     }
   };
 
-  const handleAddComment = () => {
-    // Implement the logic to add a new comment, e.g., send to the server
-    console.log('Adding new comment:', newComment);
-    try {
-        addCommentEvent();
-      setNewComment('');
-    } catch (e) {
-      console.log('Error adding new comment:', e);
-      console.error(e);
-    }
-  };
   return (
-    <div>
-      <h1>This is the comment section.</h1>
-      {comments.map((comment) => (
-        <div key={comment.id}>
-          <p>{comment.content}</p>
-          {/* Render other comment details if needed */}
-        </div>
-      ))}
+    <div className="mb-4 rounded-lg bg-white p-6 shadow-lg">
+      <h2 className="mb-4 text-2xl font-bold text-gray-800">Comments</h2>
+
+      <table className="hidden min-w-full text-gray-900 md:table">
+        <thead className="rounded-lg text-left text-sm font-normal">
+            <tr>
+                <th scope="col" className="px-3 py-5 font-medium">
+                Content
+                </th>
+                <th scope="col" className="px-3 py-5 font-medium">
+                Date
+                </th>
+            </tr>
+        </thead>
+        <tbody className="bg-white">
+          {comments.map((comment) => (
+            <tr key={comment.id} className="w-full border-b py-3 text-sm last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg">
+              <td className="whitespace-nowrap px-3 py-3">{comment.content}</td>
+              <td className="whitespace-nowrap px-3 py-3">{formatDateToLocal(comment.timestamp)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
       <div>
-        <h2>Add a new comment</h2>
-        <input
-          type="text"
-          value={newComment}
-          onChange={(e) => setNewComment(e.target.value)}
-          placeholder="Add a new comment..."
-        />
-        <button onClick={addCommentEvent}>Add Comment</button>
+        <div className="mt-2 flex items-center gap-2">
+          <textarea
+            ref={textareaRef}
+            placeholder="Add a comment or question here"
+            onChange={handleContentChange}
+            className="w-full rounded-md border border-gray-300 p-2 placeholder:text-gray-500"
+          ></textarea>
+          <button
+            className="rounded-md bg-miumee-color-400 px-4 py-2 text-white"
+            onClick={addCommentEvent}
+          >
+            Add Commment
+          </button>
+        </div>
       </div>
     </div>
   );
