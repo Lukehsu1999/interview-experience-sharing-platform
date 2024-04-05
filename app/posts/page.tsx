1ad49f559a7ed2652d17d948a5411e5933a0366d
Daniel Cha <dcha1426@gmail.com>
2024-04-05 16:23:37 -0400

    Added hot and new button, need to adjust sql in data.js

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,7 +1,7 @@
 import Pagination from '@/app/ui/posts/pagination';
 import Search from '@/app/ui/search';
 import Table from '@/app/ui/posts/table';
-import { CreatePost } from '@/app/ui/posts/buttons';
+import { CreatePost, SortButtons } from '@/app/ui/posts/buttons';
 import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
@@ -20,6 +20,7 @@ export default async function Page({
   searchParams?: {
     query?: string;
     page?: string;
+    sort?: string;
   };
 }) {
   // get user id
@@ -39,7 +40,9 @@ export default async function Page({
   // get query params
   const query = searchParams?.query || '';
   const currentPage = Number(searchParams?.page) || 1;
+  const sortMethod = searchParams?.sort || 'hot';
   const totalPages = await fetchPostsPages(query);
+
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
@@ -53,12 +56,14 @@ export default async function Page({
       </div>
       <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
         <Search placeholder="Search posts by Company, Title, Status, Type..." />
+        <SortButtons currentSort={sortMethod}/>
         <CreatePost />
       </div>
       <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
         <Table
           query={query}
           currentPage={currentPage}
+          sortMethod={sortMethod}
           viewer_id={userId}
           unlimitedView={unlimitedView}
         />
